f3d5e8633c6e5bd0c69d5c8ccdc23be5fd92a07d
elypaolazz <elisa.paolazzi-2@studenti.unitn.it>
2021-05-02 14:04:33 +0200

    Add collector.py, main.py

---
+++ collector.py
@@ -0,0 +1,31 @@
+from __future__ import absolute_import, annotations
+import requests
+import pandas as pd
+
+def get_data(filename: str):
+    data = pd.read_csv(filename)
+    return print(data.head())
+
+get_data("Tasso_occupazione.csv")
+get_data("Tasso_disoccupazione.csv")
+
+def clean_data(filename: str):
+    data = pd.read_csv(filename)
+    # rename value of occupazione/dispccupazione
+    if filename == "Tasso_occupazione.csv":
+        renamed_data = data.rename(columns={'Value': 'Tasso di occupazione'})
+        renamed_data.to_csv(filename, index=None)
+    elif filename == "Tasso_disoccupazione.csv":
+        renamed_data = data.rename(columns={'Value': 'Tasso di disoccupazione'})
+        renamed_data.to_csv(filename, index=None)
+    # rename with NUTS3 code
+    renamed_location = data.rename(columns={'ITTER107': 'NUTS3'})
+    renamed_location.to_csv(filename, index=None)
+    # delete Flag Code & Flags
+    data.drop(['Flag Codes', 'Flags'], inplace=True, axis=1)
+
+
+    print(data.head())
+
+clean_data("Tasso_occupazione.csv")
+
