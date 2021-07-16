d8657f1129c726c3c9f33884a22b510ce9b81d23
DESKTOP-B2QPHF7\lucia <luciahrovatin@gmail.com>
2021-07-16 14:55:59 +0200

    runner changed

--- runner.py
+++ runner.py
@@ -8,13 +8,13 @@ url = 'http://localhost:8080/api/v1/pools'
 r = requests.get(url, auth=('airflow', 'airflow'))
 
 for dag_name in ["ingestion_phase", "etl_phase", "mySQL_phase"]:
-    url = 'http://localhost:8080/api/v1/dags/'+dag_name+'/dagRuns'
+    url = 'http://localhost:8080/api/v1/dags/' + dag_name + '/dagRuns'
     headers = {'Content-Type': 'application/json', 'Cache-Control': 'no-cache'}
     r = requests.post(url, headers=headers, data="{}", auth=('airflow', 'airflow'))
     run = True
     allowed_state = 'success'
     while run:
-        url = 'http://localhost:8080/api/v1/dags/'+dag_name+'/dagRuns'
+        url = 'http://localhost:8080/api/v1/dags/' + dag_name + '/dagRuns'
         r = requests.get(url, auth=('airflow', 'airflow'))
         result = r.json()
         for entry in result["dag_runs"]:
@@ -25,10 +25,10 @@ for dag_name in ["ingestion_phase", "etl_phase", "mySQL_phase"]:
             time.sleep(20)
 
 cursor_Mysql = MySQLManager(host="localhost",
-                             port=3310,
-                             user="root",
-                             password="password",
-                             database="project_bdt")
+                            port=3310,
+                            user="root",
+                            password="password",
+                            database="project_bdt")
 
 cursor_Mysql.label_irpef(table_name="final")
 cursor_Mysql.label_irpef(table_name="final_individual")
@@ -40,12 +40,5 @@ redis_training(saver=cursor_Mysql, table="final_individual", case=3)
 redis_training(saver=cursor_Mysql, table="final_individual", case=3, sex=True)
 
 # Connecting to flask
-os.system('cmd /c "set FLASK_APP=main.py"')
-os.system('cmd /k "flask run"')
-
-
-
-
-
-
-
+os.environ['FLASK_APP'] = 'main.py'
+os.system("flask run")
